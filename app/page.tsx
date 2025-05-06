9dfae3d6c79de21595281af7d75f491f11cd1f28
Patrick Cui <cuiboy@Mac.cable.rcn.com>
2025-05-06 02:16:35 -0400

    finalized colors

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-empty">
-      <div className="w-[100px] h-[100px] bg-brand-primary"></div>
+      <div className="w-[100px] h-[100px] bg-brand-primary flex items-center justify-center text-body-l">
+        Hello
+      </div>
     </div>
   );
 }
